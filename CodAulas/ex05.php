a800dd3db8f6d22735c7fca4f6825a068e573c7b
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-03-17 09:10:46 -0300

    Add files via upload

---
+++ CodAulas/ex05.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<h1>3DAW</h1>
+<?php
+echo "<h2> Exercicio ARRAY </h2>";
+
+$disciplinas = array("3DAW", "3ALG", "3RSD");
+var_dump($disciplinas);
+
+
+echo "<br>";
+echo $disciplinas[0];
+echo "<br>";
+echo $disciplinas[1];
+echo "<br>";
+echo $disciplinas[2];
+
+
+?>
+
+</body>
+</html>
