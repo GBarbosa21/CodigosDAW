2317013b940e0457582fe0519f29fd4a897eb72e
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-03-17 10:38:50 -0300

    Add files via upload

---
+++ CodAulas/ex09Math.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<h1>3DAW</h1>
+<?php
+echo "<h2> Exercicio MATH </h2>";
+
+$variavel = 8.99;
+var_dump(is_int($variavel));
+echo $variavel;
+echo "<br>";
+echo pi();
+echo "<br>";
+$inteiros = array (12, 13, 44, 89, 81,19);
+echo "O maior valor é: " . max(12, 13, 44, 89, 81,19);
+echo "<br>";
+echo "O menor valor é: " . min(12, 13, 44, 89, 81,19);
+echo "<br>";
+echo "O maior valor do array é: " . max($inteiros);
+
+?>
+
+</body>
+</html>
