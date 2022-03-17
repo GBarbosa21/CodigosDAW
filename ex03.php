887d275215fa530d53c3f022f303d46cc1cc6886
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-03-17 08:18:00 -0300

    Add files via upload

---
+++ ex03.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<h1>3DAW</h1>
+<?php
+echo "<h2> Exercicio 03 </h2>";
+echo "Alo alo mundo";
+
+$variavel = "Esta é uma variavel tipo string";
+
+echo "<br>";
+echo $variavel;
+
+$variavel = 12*2;
+echo "<br>";
+echo $variavel;
+
+?>
+
+</body>
+</html>
