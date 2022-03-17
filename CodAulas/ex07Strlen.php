144a799c3973064b72ccf13d2c73f0a98a12193d
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-03-17 10:24:18 -0300

    Add files via upload

---
+++ CodAulas/ex07Strlen.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<h1>3DAW</h1>
+<?php
+echo "<h2> Exercicio 07 </h2>";
+
+$variavel = "Voce pensa que o flamengo e time?";
+var_dump(is_int($variavel))
+/*echo $variavel;
+echo "<br>";
+echo "Quantidade de caracteres: " . strlen ($variavel);
+echo "<br>";
+echo "Quantidade de palavras: " . str_word_count ($variavel);
+echo "<br>";
+echo "frase invertida: " . strrev ($variavel);
+*/
+
+?>
+
+</body>
+</html>
