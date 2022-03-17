fb8e4139395797e3fcd256392b0fb0a3a7ff6fab
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-03-17 10:47:05 -0300

    Add files via upload

---
+++ CodAulas/ex10Ops.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<h1>3DAW</h1>
+<?php
+echo "<h2> Exercicio Operadores </h2>";
+
+$var1 = 22;
+$var2 = 2;
+
+echo "Variavel 1: ". $var1;
+echo "<br>";
+echo "Variavel 2: ". $var2;
+echo "<br>";
+echo "SOMA: ";
+echo $var1 + $var2;
+echo "<br>";
+echo "SUBTRACAO: "; 
+echo $var1 - $var2;
+echo "<br>";
+echo "DIVISAO: " . $var1 / $var2;
+echo "<br>";
+echo "MOD: " . $var1 % $var2;
+echo "<br>";
+echo "EXPONENCIACAO: " . $var1 ** $var2;
+
+?>
+
+</body>
+</html>
